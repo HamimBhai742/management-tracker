78cae375241066a74557f9aa86756309184c33f5
HamimBhai742 <mdhamim5088@gmail.com>
2026-02-26 09:31:14 +0600

    Update for send email

--- src/app/bullMQ/workers/mailWorkers.ts
+++ src/app/bullMQ/workers/mailWorkers.ts
@@ -1,5 +1,5 @@
-import {  redisOptions } from "../../lib/redis/redisOptions";
-import { Worker } from "bullmq";
+import { redisOptions } from "../../lib/redis/redisOptions";
+import { Job, Worker } from "bullmq";
 import { twoFactorOtpTemplate } from "../../utils/emailTemplates/twoFactorOtpTemplate";
 import { passwordChangedTemplate } from "../../utils/emailTemplates/passwordChangedTemplate";
 import { passwordResetTemplate } from "../../utils/emailTemplates/passwordResetTemplate";
@@ -14,73 +14,82 @@ import { forgetPasswordOtpTemplate } from "../../utils/emailTemplates/forgetPass
 
 export const otpEmailWorker = new Worker(
   "otp-queue-email",
-  async (job) => {
-    switch (job.name) {
-      case "forgetPasswordOtp": {
-        const { userName, email, otpCode, subject } = job.data;
-        await forgetPasswordOtpTemplate(
-          userName,
-          subject,
-          email,
-          otpCode,
-        );
-        return "Otp end job completed";
+  async (job: Job) => {
+    try {
+      if (job.deferredFailure) {
+        console.log(`❌ Job ${job.id} was discarded or already processed.`);
+        return;
       }
-      // handle verify
-      case "registrationOtp": {
-        const { userName, email, otpCode, subject } = job.data;
-        await registrationOtpTemplate(userName, subject, email, otpCode);
-        return "Otp end job completed";
-      }
-      case "loginOtp": {
-        const { userName, email, otpCode, subject } = job.data;
-        await loginOtpTemplate(userName, subject, email, otpCode);
-        return "Otp end job completed";
-      }
-      case "twoFactorOtp": {
-        const { userName, email, otpCode, subject } = job.data;
-        await twoFactorOtpTemplate(userName, subject, email, otpCode);
-        return "Otp end job completed";
-      }
-      case "passwordChangedConfirmation": {
-        const { userName, email, subject, secureLink } = job.data;
-        await passwordChangedTemplate(userName, subject, email, secureLink);
-        return "Otp end job completed";
-      }
-      case "passwordResetRequest": {
-        const { userName, email, subject, otpCode } = job.data;
-        await passwordResetTemplate(userName, subject, email, otpCode);
-        return "Otp end job completed";
-      }
-      case "parentApprovalOtp": {
-        const { userName, email, subject, otpCode } = job.data;
-        await parentApprovalOtpTemplate(userName, subject, email, otpCode);
-        return "Otp end job completed";
-      }
-      case "resetPasswordSuccess": {
-        const { userName, email, subject, loginLink } = job.data;
-        await passwordResetSuccessTemplate(userName, subject, email, loginLink);
-        return "Otp end job completed";
-      }
-      case "adminSupport":{
-        const { adminEmail, ticket } = job.data;
-        await supportAdminTemplate(adminEmail, ticket);
-        return "Otp end job completed";
-      }
-      case "autoReplySupport":{
-        const { userEmail, userName, ticketId } = job.data;
-        await supportAutoReplyTemplate(userEmail, userName, ticketId);
-        return "Otp end job completed";
-      }
-      case "supportTicketClosed":{
-        const { userEmail, userName, ticketId } = job.data;
-        await supportClosedTemplate(userEmail, userName, ticketId);
-        return "Otp end job completed";
-      }
+      switch (job.name) {
+        case "forgetPasswordOtp": {
+          const { userName, email, otpCode, subject } = job.data;
+          await forgetPasswordOtpTemplate(userName, subject, email, otpCode);
+          return "Otp end job completed";
+        }
+        // handle verify
+        case "registrationOtp": {
+          const { userName, email, otpCode, subject } = job.data;
+          await registrationOtpTemplate(userName, subject, email, otpCode);
+          return "Otp end job completed";
+        }
+        case "loginOtp": {
+          const { userName, email, otpCode, subject } = job.data;
+          await loginOtpTemplate(userName, subject, email, otpCode);
+          return "Otp end job completed";
+        }
+        case "twoFactorOtp": {
+          const { userName, email, otpCode, subject } = job.data;
+          await twoFactorOtpTemplate(userName, subject, email, otpCode);
+          return "Otp end job completed";
+        }
+        case "passwordChangedConfirmation": {
+          const { userName, email, subject, secureLink } = job.data;
+          await passwordChangedTemplate(userName, subject, email, secureLink);
+          return "Otp end job completed";
+        }
+        case "passwordResetRequest": {
+          const { userName, email, subject, otpCode } = job.data;
+          await passwordResetTemplate(userName, subject, email, otpCode);
+          return "Otp end job completed";
+        }
+        case "parentApprovalOtp": {
+          const { userName, email, subject, otpCode } = job.data;
+          await parentApprovalOtpTemplate(userName, subject, email, otpCode);
+          return "Otp end job completed";
+        }
+        case "resetPasswordSuccess": {
+          const { userName, email, subject, loginLink } = job.data;
+          await passwordResetSuccessTemplate(
+            userName,
+            subject,
+            email,
+            loginLink,
+          );
+          return "Otp end job completed";
+        }
+        case "adminSupport": {
+          const { adminEmail, ticket } = job.data;
+          await supportAdminTemplate(adminEmail, ticket);
+          return "Otp end job completed";
+        }
+        case "autoReplySupport": {
+          const { userEmail, userName, ticketId } = job.data;
+          await supportAutoReplyTemplate(userEmail, userName, ticketId);
+          return "Otp end job completed";
+        }
+        case "supportTicketClosed": {
+          const { userEmail, userName, ticketId } = job.data;
+          await supportClosedTemplate(userEmail, userName, ticketId);
+          return "Otp end job completed";
+        }
 
-      case "resendParentOtp":
-        // handle resend
-        break;
+        case "resendParentOtp":
+          // handle resend
+          break;
+      }
+    } catch (err) {
+      console.error(`❌ Job ${job.id} failed:`, err);
+      throw err;
     }
   },
   { connection: redisOptions },
